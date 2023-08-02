b640387c55988724443bf94b990b8b3d36b01865
jduncan017 <emailjoshduncan@gmail.com>
2023-08-02 03:03:59 -0600

    ad mobile features

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import logoPath from "../../images/Logo.svg";
 import avatar from "../../images/avatar.png";
+import openMenuIcon from "../../images/hamburger-icon.png"; // Add your own path
+import closeMenuIcon from "../../images/close-icon-black.png"; // Add your own path
 
 function getCurrentDate() {
   const currentDate = new Date().toLocaleString("default", {
@@ -12,6 +14,12 @@ function getCurrentDate() {
 }
 
 const Header = ({ handleClick, weatherData }) => {
+  const [isMobileMenuOpened, setMobileMenuOpened] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpened(!isMobileMenuOpened);
+  };
+
   return (
     <header className="header">
       <div className="header__title-container">
@@ -20,17 +28,44 @@ const Header = ({ handleClick, weatherData }) => {
           {`${getCurrentDate()}, ${weatherData.location}`}{" "}
         </div>
       </div>
-      <div className="header__navigation-container">
-        <button className="header__add-clothes" onClick={handleClick}>
+      <div
+        className={`header__navigation-container ${
+          isMobileMenuOpened ? "header__navigation-container_opened" : ""
+        }`}
+      >
+        <button
+          className={`header__add-clothes ${
+            isMobileMenuOpened ? "header__add-clothes_visible" : ""
+          }`}
+          onClick={handleClick}
+        >
           + Add Clothes
         </button>
-        <div className="header__username">Joshua Duncan</div>
-        <img
-          src={avatar}
-          alt="user avatar"
-          className="header__user-avatar"
-        ></img>
+        <div
+          className={`header__user-info-container ${
+            isMobileMenuOpened ? "header__user-info-container_opened" : ""
+          }`}
+        >
+          <div className="header__username">Joshua Duncan</div>
+          <img
+            src={avatar}
+            alt="user avatar"
+            className="header__user-avatar"
+          ></img>
+        </div>
       </div>
+      <button
+        onClick={toggleMobileMenu}
+        className="header__mobile-menu"
+        style={{
+          backgroundImage: `url(${
+            isMobileMenuOpened ? closeMenuIcon : openMenuIcon
+          })`,
+        }}
+        aria-label={isMobileMenuOpened ? "Close menu" : "Open menu"}
+      >
+        {/* <img alt="menu" /> */}
+      </button>
     </header>
   );
 };

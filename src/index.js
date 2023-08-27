40f8341fbf67461d73fc8a923fabd6677c72dbab
jduncan017 <emailjoshduncan@gmail.com>
2023-08-26 18:42:56 -0600

    update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter, Switch } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <App />
+      </Switch>
+    </HashRouter>
   </React.StrictMode>
 );
 
